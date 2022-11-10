ae1fffc889baf74895c25d692e0788a3f00b141f
=Sergio <=bni.sergio@gmail.com>
2022-11-10 17:23:12 +0100

    inicio

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function addText() {
+  let div = document.getElementsByClassName('box');
+
+  for (let i = 0; i < div.length; i++) {}
+}
+
+function action() {
+  let act1 = document.getElementById('a1');
+  let act2 = document.getElementById('a2');
+
+  if (act1.hidden) {
+    act1.hidden = true;
+    act2.hidden = false;
+  } else {
+    act1.hidden = true;
+    act2.hidden = false;
+  }
+}
